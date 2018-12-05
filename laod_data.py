be4afc6529a6adec8081500776c907976e80690d
Lucien0907 <taowei.c@gmail.com>
2018-12-05 17:08:10 +0000

    first commit

---
+++ laod_data.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Spyder Editor
+
+This is a temporary script file.
+"""
+
+from scipy.io import loadmat
+
+import json
+
+# Load images data
+camId = loadmat('cuhk03_new_protocol_config_labeled.mat')['camId'].flatten()
+filelist = loadmat('cuhk03_new_protocol_config_labeled.mat')['filelist'].flatten()
+labels = loadmat('cuhk03_new_protocol_config_labeled.mat')['labels'].flatten()
+gallery_idx = loadmat('cuhk03_new_protocol_config_labeled.mat')['gallery_idx'].flatten()
+query_idx = loadmat('cuhk03_new_protocol_config_labeled.mat')['query_idx'].flatten()
+train_idxs = loadmat('cuhk03_new_protocol_config_labeled.mat')['train_idx'].flatten()
+
+# Load features data
+with open('feature_data.json', 'r') as f:
+    features = json.load(f)
+    
