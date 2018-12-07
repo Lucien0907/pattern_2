78a017778453c5ccce7f01b59e5934639e2a69c7
taowei.c <taowei.c@gmail.com>
2018-12-07 17:06:31 +0000

    Add files via upload

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ import json
 camId = loadmat('cuhk03_new_protocol_config_labeled.mat')['camId'].flatten()
 filelist = loadmat('cuhk03_new_protocol_config_labeled.mat')['filelist'].flatten()
 labels = loadmat('cuhk03_new_protocol_config_labeled.mat')['labels'].flatten()
-gallery_idx, = loadmat('cuhk03_new_protocol_config_labeled.mat')['gallery_idx,'].flatten()
+gallery_idx = loadmat('cuhk03_new_protocol_config_labeled.mat')['gallery_idx'].flatten()
 query_idx = loadmat('cuhk03_new_protocol_config_labeled.mat')['query_idx'].flatten()
 train_idx = loadmat('cuhk03_new_protocol_config_labeled.mat')['train_idx'].flatten()
 
